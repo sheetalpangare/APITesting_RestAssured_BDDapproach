3a9081646df55c75c09d791f59bd8010a6e1cb46
sheetalpangare <pangaresheetal@gmail.com>
2025-06-13 02:10:07 -0400

    Runner.java

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -1,5 +1,13 @@
-package Runner;
-
-public class Runner {
-
-}
+//package Runner;
+//
+//import org.junit.runner.RunWith;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
+//
+//@RunWith(Cucumber.class)
+//
+//@CucumberOptions(features="src/test/resources/features",glue= {"stepDefinitions"})
+//
+//public class Runner {
+//
+//}
